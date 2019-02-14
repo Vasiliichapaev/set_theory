8cc7fb7665aa72eae0436f2d927b4cb3be207cc0
Василий Чапаев <chapaevpustota@gmail.com>
2019-02-14 21:39:46 +0300

    C20

---
+++ math_core/deductiv/C20.js
@@ -0,0 +1,34 @@
+const { negation, disjunction } = require("../math_objects");
+const conjunction = require("../abbreviations/conjunction");
+const S1 = require("../schemes/S1");
+const S3 = require("../schemes/S3");
+const S4 = require("../schemes/S4");
+const C1 = require("./C1");
+const C6 = require("./C6");
+const C9 = require("./C9");
+const C12 = require("./C12");
+
+// Если A, B - теоремы, то 'A и B' тоже есть теорема
+
+exports = module.exports = (A, B) => {
+    if (A.is_ratio && B.is_ratio) {
+        if (A.verity && B.verity) {
+            let ratio = conjunction(A, B);
+            if (!ratio.verity) {
+                let neA = negation(A);
+                let neB = negation(B);
+                C9(A, B);
+                let sc = C12(A, B);
+                C1(sc);
+                sc = S4(neB, neA, neA);
+                C1(sc);
+                S1(neA);
+                C6(disjunction(neA, neB), disjunction(neA, neA), neA);
+                sc = S3(negation(disjunction(neA, neB)), neA);
+                sc = C1(sc);
+                C1(sc);
+            }
+            return ratio;
+        }
+    }
+};
--- math_core/main.js
+++ math_core/main.js
@@ -8,7 +8,7 @@ const equivalence = require("./abbreviations/equivalence");
 const C17 = require("./deductiv/C17");
 const C15 = require("./deductiv/C15");
 
-const C18 = require("./deductiv/C18");
+const C20 = require("./deductiv/C20");
 const S7 = require("./schemes/S7");
 
 const T = theory();
@@ -20,11 +20,10 @@ A = belong(a, b);
 B = belong(b, a);
 C = belong(b, b);
 
-disjunction(A, B).axiom();
-implication(A, C).axiom();
-implication(B, C).axiom();
+A.axiom();
+B.axiom();
 
-w = C18(A, B, C);
+w = C20(A, B);
 
 console.log(w.verity);
 
